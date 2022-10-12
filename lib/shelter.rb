e77c55dfd9a26206c845da32a3ba7a8334a2e6ce
Kerynn Davis <kerynn.davis@gmail.com>
2022-10-12 09:00:49 -0700

    Define add_pet

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -2,7 +2,7 @@ class Shelter
   attr_reader :name,
               :capacity
 
-  def initialize (name, capacity)
+  def initialize(name, capacity)
     @name = name
     @capacity = capacity
   end
@@ -10,5 +10,10 @@ class Shelter
   def pets
     pets = []
   end
+
+  def add_pet(pet_name)
+    puts shelter.pets << pet_name
+  end
+
 
 end
