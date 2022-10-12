575756f78577964091d3f3ee670b4cc872a0895c
Kerynn Davis <kerynn.davis@gmail.com>
2022-10-12 08:26:03 -0700

    Initial commit

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,2 +1,14 @@
 class Shelter
+  attr_reader :name,
+              :capacity
+
+  def initialize (name, capacity)
+    @name = name
+    @capacity = capacity
+  end
+
+  def pets
+    pets = []
+  end
+
 end
